c4a5c58529bfe20f885b8551bae3fea4e3f86fdf
Jguinayen <johana.guinayen@yahoo.com>
2015-12-18 16:01:46 +1300

    transfer codes from my off git workfile 181215 4:01 pm

--- WebSites/Petshopv2/BookPet.aspx.cs
+++ WebSites/Petshopv2/BookPet.aspx.cs
@@ -9,6 +9,29 @@ public partial class BookPet : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        TXTBXDATE.Text = DateTime.Now.ToShortDateString();
+        if (IsPostBack != null)
+        {
+            TXTBXBOOKINGNO.Text = Session["BookingNo"].ToString();
+            TXTBXCUSTOMERID.Text = Session["CustomerID"].ToString();
+            TXTBXCUSTOMERNAME.Text = Session["CustomerName"].ToString();
+        }
+    }
+    protected void BTNBOOK_Click(object sender, EventArgs e)
+    {
+        string PetNumber = DRPPETNUMBER.SelectedItem.Text;
+        Session["PetNumber"] = PetNumber;
+        string Groomer = DRPGROOMER.SelectedItem.Text;
+        Session["Groomer"] = Groomer;
+        string CustomerID = TXTBXCUSTOMERID.Text;
+        Session["CustomerID"] = CustomerID;
+        string Branch = DRPBRANCH.SelectedItem.Text;
+        Session["Branch"] = Branch;
+        string BookingNo = TXTBXBOOKINGNO.Text;
+        Session["BookingNo"] = BookingNo;
+        string CustomerName = TXTBXCUSTOMERNAME.Text;
+        Session["CustomerName"] = CustomerName;
 
+        Response.Redirect("BookPetPopUp.aspx");
     }
 }
